da919317c9e2598fe23fba8adadb3a44a61bb18b
Destiny <dejenkins@justworks.com>
2024-03-28 21:40:25 -0400

    updates and additions to blog.html, blog.js to assist with functionality. Also added in some styling

---
+++ assets/js/blog.js
@@ -0,0 +1,32 @@
+const storedPosts = JSON.parse(localStorage.getItem("blogPosts"));
+console.log(storedPosts,localStorage.getItem("blogPosts"));
+
+const blogPosts = storedPosts || []
+
+const blogContent = document.getElementById("content");
+
+for (let index = 0; index < blogPosts.length; index++) {
+    const post = blogPosts[index];
+
+    console.log(post);
+
+    let article = document.createElement("article")
+    let title = document.createElement("h2")
+    let username = document.createElement("p")
+    let content = document.createElement("p")
+
+    title.textContent = post.title;
+    content.textContent = post.content;
+    username.textContent = "Posted by: " + post.username;
+
+   article.appendChild(title);
+   article.appendChild(content);
+   article.appendChild(username);
+
+   blogContent.appendChild(article);
+
+}
+
+
+
+
---
+++ assets/js/form.js
@@ -0,0 +1,25 @@
+const username = document.getElementById("username");
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+  const post = {
+    username: username.value,
+    title: title.value,
+    content: content.value,
+  };
+// get the blog post array from local storage
+const storedPosts = JSON.parse(localStorage.getItem("blogPosts"));
+console.log(storedPosts,localStorage.getItem("blogPosts"));
+// parse the array, turn the string back into a data structure
+let blogPosts = storedPosts || [];
+// add our new post into the array
+blogPosts.push(post);
+// put the post array back in local storage
+localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+window.location.href = "blog.html";
+
+});
